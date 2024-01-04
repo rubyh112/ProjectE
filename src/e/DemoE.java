b1b34deeaac94b68b606c0881775c3ab6ccda976
rubyh112 <155387426+rubyh112@users.noreply.github.com>
2024-01-03 20:46:43 -0600

    Update DemoE.java

--- src/e/DemoE.java
+++ src/e/DemoE.java
@@ -3,5 +3,9 @@ package e;
 public class DemoE {
 public static void main(String[] args) {
 	System.out.println("team 2");
+	public static void add(int e , int c)	{
+	system.out.println(e+c);
+		
+			
 }
 }
